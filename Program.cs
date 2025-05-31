3f52bae837461c1f63303a00bdffd2489e841124
smoqx <smoqxpl@gmail.com>
2025-05-31 18:13:36 +0200

    Przenisiono logike do logowani jako service musi zwracac bool

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<User>();
 builder.Services.AddSingleton<User>();
 
